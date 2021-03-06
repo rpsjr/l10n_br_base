63756d882bbcdd54040a02c638382fa7e383dd3d
RPSJR <raimundops.jr@gmail.com>
2021-03-06 13:29:38 -0300

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{  # pylint: disable=C8101,C8103
+    'name': 'Odoo Next - Brazilian Base',
+    'description': 'Create a base for brazilian modules',
+    'version': '13.0.1.0.0',
+    'category': 'Localization',
+    'author': 'Trustcode',
+    'license': 'Other OSI approved licence',
+    'website': 'http://www.odoo-next.com,br',
+    'contributors': [
+        'Danimar Ribeiro <danimaribeiro@gmail.com>',
+    ],
+    'depends': [
+    ],
+    'data': [
+        "data/res.bank.csv",
+        'views/res_country.xml',
+        'views/res_company.xml',
+        "views/res_bank_view.xml",
+        "views/res_partner_bank_view.xml",
+    ],
+}
---
+++ models/__init__.py
@@ -0,0 +1,4 @@
+from . import res_country
+from . import res_company
+from . import res_bank
+from . import res_partner_bank
